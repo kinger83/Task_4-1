79c831a73265a3d357b4b6902b10e0a03214ea0d
kinger83 <kinger83@outlook.com>
2023-03-19 20:47:38 +1100

    running, but shit

--- app/src/main/java/com/example/workouttimer/WorkOutActivity.java
+++ app/src/main/java/com/example/workouttimer/WorkOutActivity.java
@@ -8,6 +8,7 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.DecimalFormat;
 import java.text.NumberFormat;
@@ -17,8 +18,8 @@ public class WorkOutActivity extends AppCompatActivity {
     public int totWorkoutTime = 20;
     public int totRestTime = 20;
 
-    int remainingSets = totSets;
-    int remaingWorkoutTime = totWorkoutTime;
+    int remainingSets = -1;
+    int remainWorkoutTime = totWorkoutTime;
     int remainingRestTime = totRestTime;
     public boolean workoutStarted = false;
 
@@ -29,12 +30,13 @@ public class WorkOutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_work_out);
         Intent intent = getIntent();
         totSets = intent.getIntExtra("sets", 0);
+        remainingSets = totSets;
         totWorkoutTime = intent.getIntExtra("workout", 0);
         totRestTime = intent.getIntExtra("rest", 0);
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
 
         TextView setView = findViewById(R.id.setValueTxt);
@@ -57,8 +59,7 @@ public class WorkOutActivity extends AppCompatActivity {
     }
 
 
-
-    public void workout(View view){
+    public void workout(View view) {
 
         // declare variables
         Button btn = findViewById(R.id.btnBeginWorkout);
@@ -66,44 +67,46 @@ public class WorkOutActivity extends AppCompatActivity {
         TextView workoutRemaining = findViewById(R.id.activeWorkoutValue);
         TextView restRemaining = findViewById(R.id.activeRestValue);
 
+//        for (int i = 0; i < remainingSets; i++) {
+//
+//            Toast testToast = Toast.makeText(getApplicationContext(), "here", Toast.LENGTH_SHORT);
+//            testToast.show();
+//        }
+
+
         CountDownTimer workoutTimer = new CountDownTimer((totWorkoutTime * 1000L), 1) {
             @Override
             public void onTick(long millisUntilFinished) {
                 NumberFormat f = new DecimalFormat("00");
                 long sec = (millisUntilFinished / 1000);
+                //remainWorkoutTime -= (millisUntilFinished * 1000);
                 workoutRemaining.setText((f.format(sec)));
             }
 
             @Override
             public void onFinish() {
-                finish();
+                remainingSets--;
+                setsRemaining.setText(String.valueOf(remainingSets));
+
             }
         }.start();
-
-
-        if(workoutStarted == false){
-            for (int i = 1; i <= totSets; i++) {
-               workoutTimer.start();
-
-
-
-
-
-                // Make button pause timer
-                // update progress value
-                // update progress display every second
-
-                // -1 on remaining workout details
-
-
-                // Make button pause timer
-                // update progress value
-                // update progress display every second
-
-
-            // loop set count times
-            }
-        }
-
     }
-}
+
+} // end class
+
+
+
+
+// Make button pause timer
+// update progress value
+// update progress display every second
+
+// -1 on remaining workout details
+
+
+// Make button pause timer
+// update progress value
+// update progress display every second
+
+
+// loop set count times
